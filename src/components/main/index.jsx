3a28a8ef3210ba2c5000a2a1cad8b2114cee4ac5
Kaue-G <kaueguedes.dev@gmail.com>
2023-04-02 11:49:36 -0300

    Template - first commit

---
+++ src/components/main/index.jsx
@@ -0,0 +1,145 @@
+import './style.sass';
+
+function Main() {
+  // eslint-disable-next-line no-console
+  console.log('chegamos aqui');
+  return (
+    <div className="main">
+      <header>
+        <section>
+          <h1>Departures</h1>
+          <img src="" alt="" />
+        </section>
+        <section>
+          <h1>La Guardia (LGA)</h1>
+        </section>
+      </header>
+      <section className="table">
+        <table>
+          <thead>
+            <tr>
+              <th>Destinations</th>
+              <th>Flight</th>
+              <th>Airline</th>
+              <th>Departure Time</th>
+              <th>Gate</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+            <tr>
+              <td>Nashville</td>
+              <td>4680</td>
+              <td>AIRLENE PHOTO</td>
+              <td>3:30 PM</td>
+              <td>Term B - 43</td>
+              <td>On Time</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+      <footer>
+        Update 4:15 PM Tuesday May 17, 2022
+      </footer>
+    </div>
+
+  );
+}
+
+export default Main;
