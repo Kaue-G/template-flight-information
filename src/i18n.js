b759b33c10d28b18a245ecdbfab91c24ebbf0cae
Kaue-G <kaueguedes.dev@gmail.com>
2023-07-02 11:47:02 -0300

    i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,12 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import ptBR from 'date-fns/locale/pt-BR';
+import enUS from 'date-fns/locale/en-US';
+import fr from 'date-fns/locale/fr';
+import es from 'date-fns/locale/es';
+import de from 'date-fns/locale/de';
+import nl from 'date-fns/locale/nl';
+import it from 'date-fns/locale/it';
 
 i18n
   .use(LanguageDetector)
@@ -8,15 +15,87 @@ i18n
     resources: {
       en: {
         translations: {
+          locale: enUS,
           Title: 'Title',
+          destinations: 'Destinations',
+          flight: 'Flight',
+          airline: 'Airline',
+          time: 'Time',
+          gate: 'Gate',
+          status: 'Status',
+          update: 'Update',
         },
       },
-      pt_br: {
+      pt: {
         translations: {
-          Title: 'Título',
-          Config: 'Configurações',
-          Media: 'Mídia',
-          Orientation: 'Orientação',
+          locale: ptBR,
+          destinations: 'Destinos',
+          flight: 'Voos',
+          airline: 'Companhia aérea',
+          time: 'Hora',
+          gate: 'Portão',
+          status: 'Situação',
+          update: 'Atualizado',
+        },
+      },
+      fr: {
+        translations: {
+          locale: fr,
+          destinations: 'Destinations',
+          flight: 'Vols',
+          airline: 'Compagnie aérienne',
+          time: 'Heure de départ',
+          gate: 'Portail',
+          status: 'Statut',
+          update: 'Mise à jour',
+        },
+      },
+      de: {
+        translations: {
+          locale: de,
+          destinations: 'Reiseziele',
+          flight: 'Flüge',
+          airline: 'Stunde',
+          time: 'Abfahrtszeit',
+          gate: 'Tor',
+          status: 'Status',
+          update: 'Aktualisieren',
+        },
+      },
+      es: {
+        translations: {
+          locale: es,
+          destinations: 'Destinos',
+          flight: 'Vuelos',
+          airline: 'Aerolínea',
+          time: 'Hora',
+          gate: 'Puerta',
+          status: 'Esttado',
+          update: 'Actualizar',
+        },
+      },
+      it: {
+        translations: {
+          locale: it,
+          destinations: 'Destinazioni',
+          flight: 'Voli',
+          airline: 'Compagnia aerea',
+          time: 'orario',
+          gate: 'Cancello',
+          status: 'Stato',
+          update: 'Aggiornamento',
+        },
+      },
+      nl: {
+        translations: {
+          locale: nl,
+          destinations: 'Bestemmingen',
+          flight: 'Vluchten',
+          airline: 'Luchtvaartmaatschappij',
+          time: 'tijd',
+          gate: 'Hek',
+          status: 'toestand',
+          update: 'Bijgewerkt',
         },
       },
     },
